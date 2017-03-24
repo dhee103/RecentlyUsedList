e19d66e9c1b65c4a44f298798c46a4903edc4613
dhee <dsg115@ic.ac.uk>
2017-03-24 14:23:02 +0000

    updated add to make sure that duplicate items are not added

--- src/main/java/ic/doc/RecentlyUsedList.java
+++ src/main/java/ic/doc/RecentlyUsedList.java
@@ -19,7 +19,9 @@ public class RecentlyUsedList {
     }
 
     public void add(String elem) {
-        list.add(0, elem);
+        if(!list.contains(elem)) {
+            list.add(0, elem);
+        }
     }
 
     public String get(int i) {
