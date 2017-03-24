f8b0648a168e677382c443916e69b0406d97c3fa
dhee <dsg115@ic.ac.uk>
2017-03-24 12:03:13 +0000

    added size field, isEmpty method and constructor to pass test that checks that list is empty when initialised

--- src/main/java/ic/doc/RecentlyUsedList.java
+++ src/main/java/ic/doc/RecentlyUsedList.java
@@ -1,5 +1,13 @@
 package ic.doc;
 
 public class RecentlyUsedList {
+    int size;
 
+    public RecentlyUsedList() {
+        this.size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
 }
